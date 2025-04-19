c2f8d4a6f0112e58544d61fd6a094640646e4144
BenKaabar <achraf.kaabar@gmail.com>
2025-04-19 19:44:50 +0100

    update v19.12

--- portfolio/src/main/java/com/example/protfolio/Security/SecurityConfig.java
+++ portfolio/src/main/java/com/example/protfolio/Security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
+                "https://portfolio-benkaabar.netlify.app",
                 "http://localhost:4200"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.ORIGIN,
